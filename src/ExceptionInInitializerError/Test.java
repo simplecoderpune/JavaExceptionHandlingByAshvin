8f75bbfc291e70aca75790ce0e3b0242982acd78
Ashvin Srivastava <59316380+ashvinsri@users.noreply.github.com>
2020-11-05 12:35:18 +0530

    Create Test.java

---
+++ src/ExceptionInInitializerError/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExceptionInInitializerError;
+
+/**
+ *
+ * @author DELL-PC
+ */
+public class Test {
+    static int x=10/0;
+    public static void main(String args[])
+    {
+        System.out.println("ExceptionInInitializerErro");
+    }
+}
